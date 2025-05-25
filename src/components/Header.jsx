21b7f14eb4a090c7bc54a16302f3f150e62de6d6
alejandronatera <alejandronatera5@gmail.com>
2025-05-25 00:01:18 -0300

    logo.jpj

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
-
-
-
 function Header() {  
-    
-    return ( 
-          
-        <header style={{ backgroundColor: "#4CAF50", padding: "0px", textAlign: "center", color: "white" }}>  
-            <h1>Bienvenidos a mi App React</h1>  
-        </header>  
-    );  
+  return (  
+    <header style={{ backgroundColor: "#003366", padding: "10px", display: "flex", alignItems: "center", color: "white" }}>  
+      <img 
+        src="/logo.jpg" 
+        alt="Logo de la página" 
+        style={{ height: "50px", marginRight: "15px" }} 
+      />  
+      <h1 style={{ margin: 0 }}>Bienvenidos a mi App React</h1>  
+    </header>  
+  );  
 }  
 
 export default Header;
