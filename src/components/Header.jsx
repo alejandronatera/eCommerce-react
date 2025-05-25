a05e929d7644c0da51ff37d885a8edd6560c83d3
alejandronatera <alejandronatera5@gmail.com>
2025-05-24 23:49:53 -0300

    public imagen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
+
+
+
 function Header() {  
     
     return ( 
+          
         <header style={{ backgroundColor: "#4CAF50", padding: "0px", textAlign: "center", color: "white" }}>  
             <h1>Bienvenidos a mi App React</h1>  
         </header>  
